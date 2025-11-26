6577cace81f495939ccb2c69e520a8ea581c0e8b
M <LXTDHT@gmail.com>
2025-11-26 10:21:40 -0500

    TypeScript array example

--- tsc-examples/todo/dist/index.js
+++ tsc-examples/todo/dist/index.js
@@ -1,2 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const todoItem_1 = require("./todoItem");
+const todoCollection_1 = require("./todoCollection");
+let todos = [
+    new todoItem_1.TodoItem(0, "Buy flowers"),
+    new todoItem_1.TodoItem(1, "Get shoes"),
+    new todoItem_1.TodoItem(2, "Collect tickets"),
+    new todoItem_1.TodoItem(3, "Call Joe", true)
+];
+let collection = new todoCollection_1.TodoCollection("Nov 26", todos);
 console.clear();
-console.log("TODO List");
+console.log(`To-Do List: ${collection.userName}`);
+let newId = collection.addTodo("Go for run");
+let todoItem = collection.getTodoById(newId);
+// console.log(JSON.stringify(todoItem));
+todoItem.printDetails();
--- tsc-examples/todo/src/index.ts
+++ tsc-examples/todo/src/index.ts
@@ -1,2 +1,18 @@
+import { TodoItem } from "./todoItem";
+import { TodoCollection } from "./todoCollection";
+
+let todos: TodoItem[] = [
+    new TodoItem(0, "Buy flowers"),
+    new TodoItem(1, "Get shoes"),
+    new TodoItem(2, "Collect tickets"),
+    new TodoItem(3, "Call Joe", true)
+];
+let collection: TodoCollection = new TodoCollection("Nov 26", todos);
+
 console.clear();
-console.log("TODO List");
+console.log(`To-Do List: ${collection.userName}`);
+
+let newId: number = collection.addTodo("Go for run");
+let todoItem: TodoItem = collection.getTodoById(newId);
+// console.log(JSON.stringify(todoItem));
+todoItem.printDetails();
