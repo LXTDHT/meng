ae8ac672f208ab7d430b062e87cbeb254266b35d
M <LXTDHT@gmail.com>
2026-01-14 18:07:10 -0500

    Started password generator project

---
+++ password-generator/index.js
@@ -0,0 +1,6 @@
+"use strict";
+const app = document.getElementById('app');
+const button = document.createElement('button');
+button.textContent = 'this is a button';
+app === null || app === void 0 ? void 0 : app.appendChild(button);
+document.body.appendChild(button);
---
+++ password-generator/index.ts
@@ -0,0 +1,5 @@
+const app = document.getElementById('app');
+const button = document.createElement('button');
+button.textContent = 'this is a button';
+app?.appendChild(button);
+document.body.appendChild(button);
